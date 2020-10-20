61d4deabe923c8491b0525193167d377cec942c8
Niphat <62160095@go.buu.ac.th>
2020-10-21 04:22:42 +0700

    Update wash_cluster8.cpp

--- source_code/wash_cluster8.cpp
+++ source_code/wash_cluster8.cpp
@@ -8,20 +8,74 @@ void employee::getdataemployee(string username,string tel)
 {
 
 }
-editclothesbasketstatus::editclothesbasketstatus()
-{
-	
+Editdryingmachine::Editdryingmachine(){
+	status = " ";
+	number = " ";
 }
-editclothesbasketstatus::editclothesbasketstatus(string day,string time,string status)
-{
-	finishDay = day;
-	finishTime = time;
-	statusClothes = status;
+void Editdryingmachine::setdata(){
+	status = st;
+	number = n;
 }
-void editclothesbasketstatus::show()
-{
-	cout<<"Username:"<<endl;
-	cout<<"Phone Number:"<<endl;
-	cout<<"Queue:"<<endl;
-	cout<<"Status:"<<finishDay<<" "<<finishTime<<" "<<status<<endl;
+string Editdryingmachine::correctstatus(string st,string n){
+	do{
+	cout<<"Enter number baking machine(1-5) : "<<endl;	
+	cin>>n;
+	}while(n<1||n>5);
+	cout<<"Enter status baking machine : "<<endl;
+	cin>>st;
+	if(n==1){
+	cout<<"------------	"<<endl;
+	cout<<"|    1"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==2){
+	cout<<"------------	"<<endl;
+	cout<<"|    2"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==3){
+	cout<<"------------	"<<endl;
+	cout<<"|    3"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==4){
+	cout<<"------------	"<<endl;
+	cout<<"|    4"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==5){
+	cout<<"------------	"<<endl;
+	cout<<"|    5"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}				
+}
+void Editdryingmachine::showBakingmachine(){
+	cout<<"------------		------------		------------        ------------		------------	"<<endl;
+	cout<<"|    1     |		|    2 	   |		|    3     |        |    4     |		|    5	   |	"<<endl;
+	cout<<"------------		------------		------------	    ------------		------------	"<<endl;
+	cout<<"|          |		|     	   |		|          |        |          |		|    	   |	"<<endl;
+	cout<<"|          |		|     	   |		|          |        |          |		|    	   |	"<<endl;
+	cout<<"|          |		|     	   |		|          |        |          |		|    	   |	"<<endl;
+	cout<<"------------		------------		------------        ------------		------------	"<<endl;
+
 }
