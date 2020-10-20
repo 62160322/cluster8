4722c7bce7377a5f024cbad4ca964d9fb376f97c
Suradej Saithongmee <62160339@go.buu.ac.th>
2020-10-20 23:33:51 +0700

    ให้ทุกคนทำต่อจากนี้
    นำคลาสแต่ละคนที่ทำทำต่อจากไฟล์นี้

---
+++ wash_cluster8.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class employee
+{
+	private:
+		string nameEmployee;
+		string telEmployee;
+	public:
+		employee();
+		void getdataemployee(string username,string tel);
+	protected:
+};
+
