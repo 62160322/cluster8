9822d4d4fd4ae4fb108e1dcd58418b142d0f4837
Nongnapat <62160090@go.buu.ac.th>
2020-10-20 20:42:12 +0700

    add class Employee and method

---
+++ employee.cpp
@@ -0,0 +1,11 @@
+#include "employee.h"
+
+employee::employee()
+{
+	nameEmployee = " ";
+	telEmployee = " ";
+}
+void employee::getdataEmployee(string username,string tel)
+{
+	
+}
---
+++ employee.h
@@ -0,0 +1,15 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+class employee
+{
+	private:
+		string nameEmployee;
+		string telEmployee;
+	public:
+		employee();
+		void getdataemployee(string username,string tel);
+	protected:
+};
+
+#endif
