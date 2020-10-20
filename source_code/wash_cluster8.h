1017abadd99a7f119bf050b02e0acb9c2ec3da81
Kanachart Intuphut <62160076@go.buu.ac.th>
2020-10-21 03:22:17 +0700

    Update wash_cluster8.h

--- source_code/wash_cluster8.h
+++ source_code/wash_cluster8.h
@@ -12,4 +12,18 @@ class employee
 		void getdataemployee(string username,string tel);
 	protected:
 };
-
+class editclothesbasketstatus
+{
+	protected:
+		string finishDay;
+		string finishTime;
+		string statusClothes;
+		clothesbasket 
+		customer c;
+		queuewashing qw;
+		queuedrying qd;
+	public:
+		editclothesbasketstatus():
+		editclothesbasketstatus(string day,string time,string status);
+		void show():
+};
