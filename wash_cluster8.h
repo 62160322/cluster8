785a7548bc0d554367bd2cff02b389f7aa1b01a0
Kanachart Intuphut <62160076@go.buu.ac.th>
2020-10-21 03:08:33 +0700

    Create wash_cluster8.h

---
+++ wash_cluster8.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class employee
+{
+	private:
+		string nameEmployee;
+		string telEmployee;
+	public:
+		employee();
+		void getdataemployee(string username,string tel);
+	protected:
+};
+class editclothesbasketstatus
+{
+	protected:
+		string finishDay;
+		string finishTime;
+		string statusClothes;
+		clothesbasket 
+		customer c;
+		queuewashing qw;
+		queuedrying qd;
+	public:
+		editclothesbasketstatus():
+		editclothesbasketstatus(string day,string time,string status);
+		void show():
+};
