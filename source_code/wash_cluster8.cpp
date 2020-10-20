1bc683ca59ab7614dd07e2d8e205d3e79d4df03b
Kanachart Intuphut <62160076@go.buu.ac.th>
2020-10-21 03:22:06 +0700

    Update wash_cluster8.cpp

--- source_code/wash_cluster8.cpp
+++ source_code/wash_cluster8.cpp
@@ -8,3 +8,20 @@ void employee::getdataemployee(string username,string tel)
 {
 
 }
+editclothesbasketstatus::editclothesbasketstatus()
+{
+	
+}
+editclothesbasketstatus::editclothesbasketstatus(string day,string time,string status)
+{
+	finishDay = day;
+	finishTime = time;
+	statusClothes = status;
+}
+void editclothesbasketstatus::show()
+{
+	cout<<"Username:"<<endl;
+	cout<<"Phone Number:"<<endl;
+	cout<<"Queue:"<<endl;
+	cout<<"Status:"<<finishDay<<" "<<finishTime<<" "<<status<<endl;
+}
