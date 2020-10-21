78b79b0c6c83bdee064daba30967219555c22875
Kanachart Intuphut <62160076@go.buu.ac.th>
2020-10-21 19:47:28 +0700

    Create ClothesBasket.cpp

---
+++ ClothesBasket.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class ClothesBasket{
+	public:
+		int weightClothes;
+		string typeJob;
+		
+		ClothesBasket(){
+			
+		}
+		void setClothesBasket(int weight,string type){
+			weightClothes = weight;
+			typeJob = type;
+		}
+		int getWeight(){
+			return weightClothes; 
+		}
+		string getType(){
+			return typeJob;
+		}
+		void ChangeStatusClothesBasket(string name,string tel){
+			
+		}
+		void CheckStatusClothesBasket(string name,string tel){
+			
+		}
+		string getDataCustomer(string name,string tel){
+			
+		}
+		int getAmountReceived(int received){
+			
+		}
+		int getBillCustomer(int choise){
+			
+		} 
+};
+
+
+
+
