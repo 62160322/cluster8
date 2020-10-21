9e9a13cd3816a7412f268e058a16cf7b6c2ed0f1
Kanachart Intuphut <62160076@go.buu.ac.th>
2020-10-21 13:59:27 +0700

    Create EditDryingmachineStatus.cpp

---
+++ EditDryingmachineStatus.cpp
@@ -0,0 +1,73 @@
+#include "EditDryingmachineStatus.h"
+
+Editdryingmachine::Editdryingmachine(){
+	status = " ";
+	number = 0;
+}
+void Editdryingmachine::setdata(){
+	status = st;
+	number = n;
+}
+string Editdryingmachine::correctstatus(string st,string n){
+	do{
+	cout<<"Enter number baking machine(1-5) : "<<endl;	
+	cin>>n;
+	}while(n<1||n>5);
+	cout<<"Enter status baking machine : "<<endl;
+	cin>>st;
+	if(n==1){
+	cout<<"------------	"<<endl;
+	cout<<"|    1"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==2){
+	cout<<"------------	"<<endl;
+	cout<<"|    2"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==3){
+	cout<<"------------	"<<endl;
+	cout<<"|    3"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==4){
+	cout<<"------------	"<<endl;
+	cout<<"|    4"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}
+	if(n==5){
+	cout<<"------------	"<<endl;
+	cout<<"|    5"<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	cout<<"|          |	"<<endl;
+	cout<<"|"<<" "<<setw(6)<<st<<setw(4)<<"|"<<setw(5)<<endl;
+	cout<<"|     "<<setw(6)<<"|"<<endl;
+	cout<<"------------	"<<endl;
+	}				
+}
+void Editdryingmachine::showBakingmachine(){
+	cout<<"------------		------------		------------        ------------		------------	"<<endl;
+	cout<<"|    1     |		|    2 	   |		|    3     |        |    4     |		|    5	   |	"<<endl;
+	cout<<"------------		------------		------------	    ------------		------------	"<<endl;
+	cout<<"|          |		|     	   |		|          |        |          |		|    	   |	"<<endl;
+	cout<<"|          |		|     	   |		|          |        |          |		|    	   |	"<<endl;
+	cout<<"|          |		|     	   |		|          |        |          |		|    	   |	"<<endl;
+	cout<<"------------		------------		------------        ------------		------------	"<<endl;
+
+}
